960b408b91e6f6c829b592fb19af8d08ec6e41e9
Carey Spies <carey.spies@alltrails.com>
2024-02-22 11:39:17 -0800

    cleanup globals

--- build/ios/brand-1/tokens.h
+++ build/ios/brand-1/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 22 Feb 2024 19:34:53 GMT
+// Generated on Thu, 22 Feb 2024 19:39:02 GMT
 
 
 #import <Foundation/Foundation.h>
@@ -15,10 +15,8 @@
 #define ColorBaseGrayLight [UIColor colorWithRed:0.800f green:0.800f blue:0.800f alpha:1.000f]
 #define ColorBaseGrayMedium [UIColor colorWithRed:0.600f green:0.600f blue:0.600f alpha:1.000f]
 #define ColorBaseGrayDark [UIColor colorWithRed:0.067f green:0.067f blue:0.067f alpha:1.000f]
-#define ColorBaseRed [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
-#define ColorBaseGreen [UIColor colorWithRed:0.000f green:1.000f blue:0.000f alpha:1.000f]
-#define ColorTextBase [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
-#define ColorTextSecondary [UIColor colorWithRed:0.000f green:1.000f blue:0.000f alpha:1.000f]
+#define ColorTextBase [UIColor colorWithRed:0.067f green:0.067f blue:0.067f alpha:1.000f]
+#define ColorTextSecondary [UIColor colorWithRed:0.600f green:0.600f blue:0.600f alpha:1.000f]
 #define ColorTextTertiary [UIColor colorWithRed:0.800f green:0.800f blue:0.800f alpha:1.000f]
 #define FontFamilyBody @"Manrope"
 #define FontFamilyDisplay @"Beatrice"
